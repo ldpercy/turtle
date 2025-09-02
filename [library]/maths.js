5be1f28a98dc2de4f4ab9f7c388505e5218c25b8
Paul Dale <ldpercy@gmail.com>
2025-09-02 19:58:46 +1000

    First go at n-ary representation

---
+++ [library]/maths.js
@@ -0,0 +1,26 @@
+//
+// Maths
+//
+
+function baseSize(base, number) {
+	power = 0;
+	while (base**power <= number) { power++; }
+	return power;
+}
+
+
+function numberToBase(number, base) {
+	let result = {};
+	let remainder = number;
+	let power = 0;
+	let placeKey = 0;
+	let placeVal = 0;
+	while (remainder > 0) {
+		placeKey = base**power;
+		placeVal = remainder % base;
+		result[placeKey] = placeVal;
+		remainder = (remainder - placeVal)/ base;
+		power++;
+	}
+	return result;
+}
