0153ae7254abcf6ced32309187da84703aef6ea0
Paul Dale <ldpercy@gmail.com>
2025-09-03 13:19:36 +1000

    Improve presentation for n-box

--- [library]/maths.js
+++ [library]/maths.js
@@ -24,3 +24,23 @@ function numberToBase(number, base) {
 	}
 	return result;
 }
+
+
+/* Similar to above but doesn't use zero
+*/
+function numberToBaseN(number, base) {
+	let result = {};
+	let remainder = number;
+	let power = 0;
+	let placeKey = 0;
+	let placeVal = 0;
+	while (remainder > 0) {
+		placeKey = base**power;
+		placeVal = ((remainder % base) === 0) ? base : remainder % base;
+
+		result[placeKey] = placeVal;
+		remainder = (remainder - placeVal)/ base;
+		power++;
+	}
+	return result;
+}
