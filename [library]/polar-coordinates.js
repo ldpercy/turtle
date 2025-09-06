fbf48ee6ad798fd75129960f532ca628fc00b96e
Paul Dale <ldpercy@gmail.com>
2025-09-06 12:36:14 +1000

    Convert point and polarPoint to classes

--- [library]/polar-coordinates.js
+++ [library]/polar-coordinates.js
@@ -1,17 +1,31 @@
 
-function Point(x, y)
-{
-	this.x = x;
-	this.y = y;
-}
+class Point {
+	constructor(x=0, y=0) {
+		this.x = x;
+		this.y = y;
+	}
 
-function polarPoint(radians, radius)
-{
-	return new Point(
-		radius * Math.sin(radians),
-		radius * -Math.cos(radians)
-	)
-}
+}/* Point */
+
+
+class PolarPoint {
+	constructor(radian, radius)
+	{
+		this.radian = radian;
+		this.radius = radius;
+	}
+
+	toPoint = function() {
+		return new Point(
+			this.radius * Math.sin(this.radian),
+			this.radius * -Math.cos(this.radian)
+		)
+	}
+
+}/* PolarPoint */
+
+
+
 
 
 const tau = 2 * Math.PI;
