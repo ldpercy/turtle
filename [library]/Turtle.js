6369b30167053d0f6669983c3bd06787741a5dd1
Paul Dale <ldpercy@gmail.com>
2025-09-27 11:11:43 +1000

    Load Turtle class

--- [library]/Turtle.js
+++ [library]/Turtle.js
@@ -1,15 +1,21 @@
 class Turtle {
 
 	#position = new Point();
-	heading = new Angle();
+	#heading = new Angle();
 	#origin = new Point(0,0);
 
 	constructor(x=0, y=0, heading=0, digits=12) {
 		this.#position.x = x;
 		this.#position.y = y;
-		this.heading.degrees = heading;
-		this.precision.digits = digits;
+		this.#heading.degrees = heading;
+		//this.precision.digits = digits;
 	}
+
+
+
+
+
+
 
 
 }
