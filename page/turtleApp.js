f282b5b265af53add7c571b13279801f8f662034
Paul Dale <ldpercy@gmail.com>
2025-10-16 18:49:19 +1100

    Done: polar grid

--- page/turtleApp.js
+++ page/turtleApp.js
@@ -8,7 +8,7 @@ class TurtleApp extends HTMLApp {
 	info = `
 		Turtle by ldpercy
 		https://github.com/ldpercy/turtle/
-		v0.6a
+		v0.6
 		https://github.com/ldpercy/turtle/pull/5
 	`.replace(/\n\t/g,'\n');
 
