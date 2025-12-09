0fbfe454ffb89b23efa1b17f91d2d7c6ecfaf5a7
Paul Dale <ldpercy@gmail.com>
2025-12-09 21:05:01 +1100

    Add kbd controls for command tabs

--- app/keyboard.js
+++ app/keyboard.js
@@ -17,8 +17,14 @@ export function documentKeyListener(event) {
 		case 'd'	: turtleApp.doCommands(); break;
 		case 'c'	: svg.clearDrawing(); break;
 		case 'o'	: turtleApp.toOrigin(); break;
-		//case '+'	: turtleApp.toOrigin(); break;
-		//case '-'	: turtleApp.toOrigin(); break;
+
+		case '!'	: ui.showCommandSet(1); break;
+		case '@'	: ui.showCommandSet(2); break;
+		case '#'	: ui.showCommandSet(3); break;
+
+		//case 'C'	: ui.showCommandSet(3); break;
+		//case '+'	: ui.zoomIn(); break;
+		//case '-'	: ui.zoomOut(); break;
 
 		default     : /* do nothing */; break;
 	}
