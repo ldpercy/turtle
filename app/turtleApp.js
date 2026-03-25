e81c31766954fa7ec1f02c2ee1a56dedcc0ffb3a
Paul Dale <ldpercy@gmail.com>
2026-03-25 18:41:08 +1100

    Done: v0.12.3

--- app/turtleApp.js
+++ app/turtleApp.js
@@ -17,7 +17,7 @@ import { ui } from './view-html-ui.js';
 class TurtleApp extends HTMLApp {
 
 	appName			= 'turtle';
-	appVersion		= 'v0.12.2';
+	appVersion		= 'v0.12.3';
 	projectColour	= 'lightseagreen';
 	appInfo = [`%c
 		Turtle ${this.appVersion} by ldpercy
