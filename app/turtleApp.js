81c448793ef0e35583bcef08894c0b311e420161
Paul Dale <ldpercy@gmail.com>
2026-04-26 13:03:12 +1000

    Done: v0.13.0 - download drawing

--- app/turtleApp.js
+++ app/turtleApp.js
@@ -17,7 +17,7 @@ import { ui } from './html-ui.js';
 class TurtleApp extends HTMLApp {
 
 	appName			= 'turtle';
-	appVersion		= 'v0.12.3';
+	appVersion		= 'v0.13.0';
 	projectColour	= 'lightseagreen';
 	appInfo = [`%c
 		Turtle ${this.appVersion} by ldpercy
